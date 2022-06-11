9805bcdedd1c00b036054b5d3419c4d8580bcee3
Alexis Bouchez <bouchezalexis1@gmail.com>
2022-06-11 17:35:01 +0200

    Add some controller

---
+++ src/Controller/BookController.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class BookController extends AbstractController
+{
+    #[Route('/book', name: 'show_book')]
+    public function index(): Response
+    {
+        return $this->render('book/index.html.twig', [
+            'controller_name' => 'BookController',
+        ]);
+    }
+
+    #[Route('/book/{id}', name: 'list_books')]
+    public function show(string $id)
+    {
+        return new Response(
+            "Book id $id"
+        );
+    }
+}
